7fad6751c1387d6e6f20ed6564085b072f71f835
dannyxlee <81855990+dannyxlee@users.noreply.github.com>
2021-08-10 20:42:06 +0900

    awrqrdsf

--- EZWorks/src/main/java/com/it/ez/configuration/MvcConfiguration.java
+++ EZWorks/src/main/java/com/it/ez/configuration/MvcConfiguration.java
@@ -16,7 +16,7 @@ import com.it.ez.controller.LoginInterceptor;
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer{
 
-	private static final List<String> URL_PATTERNS = Arrays.asList("/addrbook/*", "/board/*", "/approval/*", "/archive/*", "/attendance/*", "/calendar/*", "/community/*", "/form/*", "/message/*", "/mal/*", "/reservation/*", "/schcate/*");  //인터셉터가 동작 해야 될 요청 주소 mapping 목록
+	private static final List<String> URL_PATTERNS = Arrays.asList("/addrbook/*", "/board/*", "/approval/*", "/archive/*", "/attendance/*", "/calendar/*", "/community/*", "/form/*", "/message/*", "/mal/*", "/reservation/*", "/schcate/*", "/");  //인터셉터가 동작 해야 될 요청 주소 mapping 목록
 	
 	//인터셉터 주소 세팅
 	@Override
